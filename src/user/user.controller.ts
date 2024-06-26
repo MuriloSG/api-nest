35b0f1d2d176caac065be3f239dab5c26c34cd0c
MuriloSG <muriloprojetosti@gmail.com>
2024-06-26 10:30:05 -0300

    feat: methods get all and ById

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 @Controller("users")
 export class UserController {
   @Post()
   async create(@Body() body) {
     return { body };
+  }
+
+  @Get()
+  async getAll() {
+    return { users: [] };
+  }
+
+  @Get(":id")
+  async getById(@Param() param) {
+    return { user: {}, param };
   }
 }
