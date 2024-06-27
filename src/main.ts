99e383050b19cbb8ed196e06d3bf32af681ce73b
MuriloSG <muriloprojetosti@gmail.com>
2024-06-27 08:49:25 -0300

    feat: implement interceptor logs

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import * as dotenv from "dotenv";
+import { LogsInterceptor } from "./interceptors/log.interceptor";
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LogsInterceptor());
   const config = new DocumentBuilder()
     .setTitle("Api-scheduling")
     .setDescription("Generic scheduling API")
