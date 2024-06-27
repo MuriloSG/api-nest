b20ff9a9b5c44b09dc0f1f990c4e88134558a394
MuriloSG <muriloprojetosti@gmail.com>
2024-06-27 10:19:17 -0300

    feat: docs swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Raiz")
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: "Get a welcome message" })
+  @ApiResponse({
+    status: 200,
+    description: "Welcome message returned successfully",
+  })
   getHello(): string {
     return this.appService.getHello();
   }
