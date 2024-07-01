979f753b3688243d4814a07647b30ee9095aa5ae
MuriloSG <muriloprojetosti@gmail.com>
2024-07-01 11:02:15 -0300

    feat: upload file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
+  BadRequestException,
   Body,
   Controller,
-  Headers,
   Post,
-  Req,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
 import { AuthLoginDTO } from "./dto/auth-login.dto";
 import { AuthRegisterDTO } from "./dto/auth-register.dto";
 import { AuthForgotDTO } from "./dto/auth-forgot.dto";
 import { AuthResetDTO } from "./dto/auth-reset.dto";
-import { UserService } from "src/user/user.service";
 import { AuthService } from "./auth.service";
 import { ApiTags } from "@nestjs/swagger";
 import { AuthGuard } from "src/guards/auth.guard";
 import { User } from "src/decorators/user.decorator";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { join } from "path";
+import { FileService } from "src/file/file.service";
 
 @ApiTags("auth")
 @Controller("auth")
 export class AuthController {
   constructor(
-    private readonly userService: UserService,
     private readonly authService: AuthService,
+    private readonly fileService: FileService,
   ) {}
 
   @Post("login")
@@ -49,5 +51,25 @@ export class AuthController {
   @Post("me")
   async me(@User() user) {
     return { user };
+  }
+
+  @UseInterceptors(FileInterceptor("file"))
+  @UseGuards(AuthGuard)
+  @Post("photo")
+  async uploadPhoto(@User() user, @UploadedFile() photo: Express.Multer.File) {
+    const path = join(
+      __dirname,
+      "..",
+      "..",
+      "storage",
+      "users-photos",
+      `photo-${user.id}.jpg`,
+    );
+    try {
+      await this.fileService.upload(photo, path);
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+    return { sucess: true };
   }
 }
