5e432e69b3dea3a3106fab169cae2b4f4f0904e7
MuriloSG <muriloprojetosti@gmail.com>
2024-11-10 19:31:44 -0300

    feat: teste

--- src/decorators/param-id.decorator.ts
+++ src/decorators/param-id.decorator.ts
@@ -2,6 +2,7 @@ import { ExecutionContext, createParamDecorator } from "@nestjs/common";
 
 export const ParamId = createParamDecorator(
   (_data: unknown, context: ExecutionContext) => {
+    console.log("teste");
     return Number(context.switchToHttp().getRequest().params.id);
   },
 );
